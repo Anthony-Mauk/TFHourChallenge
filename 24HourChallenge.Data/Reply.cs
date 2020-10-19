e4fc8a24793d3175f7566ddec99c679c0e180f0a
dknauff <dknauffsd@gmail.com>
2020-10-19 15:30:00 -0400

    added replycomment

---
+++ 24HourChallenge.Data/Reply.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourChallenge.Data
+{
+    public class Reply : Comment
+    {
+        public string ReplyComment { get; set; }
+        //[ForeignKey(nameof(Comment))]
+        //public virtual Comment Comment { get; set; }
+    }
+}

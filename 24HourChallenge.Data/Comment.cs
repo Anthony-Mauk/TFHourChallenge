a6acabcf72078f287b79aaaa0016c7f82c5ff054
Jamie Williams <jlatwil@gmail.com>
2020-10-19 15:24:28 -0400

    added comment class and properties

---
+++ 24HourChallenge.Data/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourChallenge.Data
+{
+    public class Comment
+    {
+        [ForeignKey("Id")] [Required] public int Id { get; set; }
+        [Required] public string Text { get; set; }
+        [ForeignKey("Author")] [Required] public User Author { get; set; }
+        [Required] public Post CommentPost { get; set; }
+    }
+}

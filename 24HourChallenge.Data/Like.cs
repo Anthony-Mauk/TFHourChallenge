07dbfd5cceb2ce325fbadb476b4c9f77a449dfb4
ClaytonCampbell <ccampbell9764@gmail.com>
2020-10-19 15:29:57 -0400

    Like

---
+++ 24HourChallenge.Data/Like.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourChallenge.Data
+{
+    public class Like
+    {
+        public int Liked { get; set; }
+        [ForeignKey(nameof(Liked))]
+        public virtual Post LikedPost { get; set; }
+        public int LikerID { get; set; }
+        [ForeignKey(nameof(LikerID))]
+        public virtual User Liker { get; set; }
+    }
+}

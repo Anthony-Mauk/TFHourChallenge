752f0a051df6083c5277f01f88621361af851163
Jamie Williams <jlatwil@gmail.com>
2020-10-19 21:18:11 -0400

    fixed error and completed comment class.

---
+++ 24HourChallenge.Data/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourChallenge.Data
+{
+    public class Comment
+    {
+        [Required] public int Id { get; set; }
+        [Required] public string Text { get; set; }
+        [Required] public User Author { get; set; }
+        [Required] public Post CommentPost { get; set; }
+    }
+}
